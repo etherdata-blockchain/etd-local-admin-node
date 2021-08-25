5e36b42d8a78d88b6992b6cbfb0f3492506dbd9a
sirily11 <sirily1997@gmail.com>
2021-08-25 11:33:31 +0800

    Update admin node

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import { NodeClient } from "./node_client/node_client";
+import Logger         from "./logger";
 
+
+let pjson = require('../package.json');
+Logger.info(`Current version: ${pjson.version}`)
 let node = new NodeClient({});
 node.startServer();
