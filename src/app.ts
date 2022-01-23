3ca8464566bac396e8141b903e9e832f623b85c3
sirily11 <sirily1997@gmail.com>
2022-01-23 18:59:49 +0800

    fix: bugs

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,13 @@
 import { config } from "dotenv";
-import * as docker from "@pulumi/docker";
 import { NodeClient } from "./node_client/node_client";
 import Logger from "./logger";
 // @ts-ignore
 import pjson from "../package.json";
 
 (async () => {
-  // config();
-  // global.version = pjson.version;
-  // Logger.info(`Current version: ${pjson.version}`);
-  // const node = new NodeClient({});
-  // await node.startApp();
-
-  const image = new docker.RemoteImage("ubuntu", {
-    name: "ubuntu:precise",
-  });
-
-  const container = new docker.Container("ubuntu", {
-    image: image.latest,
-  });
+  config();
+  global.version = pjson.version;
+  Logger.info(`Current version: ${pjson.version}`);
+  const node = new NodeClient({});
+  await node.startApp();
 })();
--- src/node_client/node_client.ts
+++ src/node_client/node_client.ts
@@ -1,7 +1,3 @@
-import moment from "moment";
-import osu from "os-utils";
-import os from "os";
-import { SystemInfo } from "../internal/interfaces/systemInfo";
 import Logger from "../logger";
 import { PluginApp } from "../internal/handlers/basePlugin";
 import { StatusPlugin } from "../internal/handlers/status/statusPlugin";
@@ -14,36 +10,5 @@ export class NodeClient extends PluginApp {
     super();
     this.plugins = [new JobPlugin(), new StatusPlugin()];
     Logger.info("Start server");
-  }
-
-  private async prepareSystemInfo(): Promise<SystemInfo[]> {
-    const memoryFree = os.freemem();
-    const sysUpTime = osu.sysUptime();
-    const cpuUsage: number = await new Promise((resolve, reject) => {
-      osu.cpuUsage((u) => {
-        resolve(u);
-      });
-    });
-
-    return [
-      {
-        title: "CPU",
-        description: "CPU Usage",
-        value: (cpuUsage * 100).toFixed(2),
-        unit: "%",
-      },
-      {
-        title: "Mem Free",
-        description: "Memory Free",
-        value: (memoryFree / 1024 / 1024 / 1024).toFixed(2),
-        unit: "GB",
-      },
-      {
-        title: "Up Time",
-        description: "System Up Time",
-        value: moment({}).seconds(sysUpTime).format("HH:mm:ss"),
-        unit: "",
-      },
-    ];
   }
 }
