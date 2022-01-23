d31fe994ef0279a3c38099aac8e39dd82fb125ec
sirily11 <sirily1997@gmail.com>
2022-01-23 18:38:51 +0800

    feat: remove packages

--- packages/docker-plan/src/internal/const/configurations.ts
+++
@@ -1,3 +0,0 @@
-export const Configurations = {
-  defaultStackPosition: "./stack.lock.yaml",
-};
--- packages/docker-plan/src/tests/data/mock_error_msg.ts
+++
@@ -1,6 +0,0 @@
-export const MockErrors = {
-  mockPullingError: "Cannot pull this image",
-  mockContainerCreationError: "Cannot create this container",
-  mockContainerDeletionError: "Cannot delete this container",
-  mockImageDeletionError: "Cannot delete this image",
-};
